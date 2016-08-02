d2f30a6108623e078743f01e51e7dcf62b93d361
nbappi <bappi.cse562@gmail.com>
2016-08-02 11:36:23 +0600

    Prints a list of files in a given directory,   filtered by the extension of the files is updated.

--- step-4.js
+++ step-4.js
@@ -1,15 +1,18 @@
 var fs = require("fs");
+var path = require("path");
+var ext = process.argv[3];
+var folders = process.argv[2];
 
 function callback(err , data)
 {
-    if( err ) throw err ;
+    if( err ) return console.log(err) ;
 
-    for( var i = 0; i < data.length; i++){
-        var split = data[i].split('.');
-        if(split[1] == "md"){
-            console.log(data[i]);
+    data.forEach(function(file){
+        if( path.extname(file) == "."+ ext){
+            console.log(file);
         }
-    }
+
+    });
 }
 
-fs.readdir(process.argv[2] , callback);
+fs.readdir(folders , callback);
